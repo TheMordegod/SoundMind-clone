ce03c732059b945a12ab2875dcaa20988c8dcd4b
Levy <levysilva72@gmail.com>
2023-04-01 00:20:49 -0300

    adicionado animação improvisada do botão de fechar do overlay

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,17 @@
 function toggleOverlay() {
     //const button = document.querySelectorAll('hamburger__button')
     const overlayWindow = document.querySelector('.overlay')
+    const closeButtonLeft = document.querySelector('.hamburger__close-button--left')
+    const closeButtonRight = document.querySelector('.hamburger__close-button--right')
 
     if (overlayWindow.classList.contains('overlay--active')) {
         overlayWindow.classList.remove('overlay--active')
+        closeButtonLeft.classList.remove('build')
+        closeButtonRight.classList.remove('build')
         return
     }
 
-    return overlayWindow.classList.add('overlay--active')
+    overlayWindow.classList.add('overlay--active')
+    closeButtonLeft.classList.add('build')
+    closeButtonRight.classList.add('build')
 }
