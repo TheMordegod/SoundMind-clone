6eaca019b5a62d74e409ead0e73b5d6c31f90747
Levy <levysilva72@gmail.com>
2023-04-10 23:49:41 -0300

    colocado funcionalidade de rodar videos dentro do smartphone

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -44,4 +44,35 @@ function changeHeart() {
     }, 2000)
 }
 
+function showVideo() {
+    const closeVideoBtn = document.querySelector('.phone-screen__close-button')
+    const videoScreen = document.querySelector('.phone-screen__video')
+    const songs = document.querySelectorAll('.phone-screen__song')
+    const videoLinks = [
+        'https://www.soundmind.app/wp-content/themes/soundmind/assets/video/alert-and-calm/alert-and-calm.mp4',
+        'https://www.soundmind.app/wp-content/themes/soundmind/assets/video/balance/balance.mp4',
+        'https://www.soundmind.app/wp-content/themes/soundmind/assets/video/brain-power/brain-power.mp4',
+        'https://www.soundmind.app/wp-content/themes/soundmind/assets/video/deep-sleep/deep-sleep.mp4'
+    ]
+
+    songs.forEach((song, soundIndex) => {
+        song.addEventListener('click', () => {
+            closeVideoBtn.classList.add('phone-screen__video--active')
+            closeVideoBtn.focus()
+            videoScreen.classList.add('phone-screen__video--active')
+            videoScreen.src = videoLinks[soundIndex]
+            videoScreen.play()
+
+        })
+    })
+
+    closeVideoBtn.addEventListener('click', () => {
+        closeVideoBtn.classList.remove('phone-screen__video--active')
+        videoScreen.classList.remove('phone-screen__video--active')
+        videoScreen.pause()
+    })
+}
+
+
+showVideo()
 changeHeart()
