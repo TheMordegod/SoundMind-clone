bcf15090b3cf692e7afd97cd957752f35dbca92c
Levy <levysilva72@gmail.com>
2023-04-11 23:28:05 -0300

    adicionado musica e alterado codigo para tocar o audio e usar menos banda

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -46,33 +46,30 @@ function changeHeart() {
 
 function showVideo() {
     const closeVideoBtn = document.querySelector('.phone-screen__close-button')
-    const videoScreen = document.querySelector('.phone-screen__video')
+    const videoScreen = document.querySelectorAll('.phone-screen__video')
     const songs = document.querySelectorAll('.phone-screen__song')
-    const videoLinks = [
-        'https://www.soundmind.app/wp-content/themes/soundmind/assets/video/alert-and-calm/alert-and-calm.mp4',
-        'https://www.soundmind.app/wp-content/themes/soundmind/assets/video/balance/balance.mp4',
-        'https://www.soundmind.app/wp-content/themes/soundmind/assets/video/brain-power/brain-power.mp4',
-        'https://www.soundmind.app/wp-content/themes/soundmind/assets/video/deep-sleep/deep-sleep.mp4'
-    ]
+    const audios = document.querySelectorAll('.audio')
 
     songs.forEach((song, soundIndex) => {
         song.addEventListener('click', () => {
             closeVideoBtn.classList.add('phone-screen__video--active')
+            videoScreen[soundIndex].classList.add('phone-screen__video--active')
+            audios[soundIndex].play()
+            videoScreen[soundIndex].play()
             closeVideoBtn.focus()
-            videoScreen.classList.add('phone-screen__video--active')
-            videoScreen.src = videoLinks[soundIndex]
-            videoScreen.play()
-
         })
     })
 
     closeVideoBtn.addEventListener('click', () => {
         closeVideoBtn.classList.remove('phone-screen__video--active')
-        videoScreen.classList.remove('phone-screen__video--active')
-        videoScreen.pause()
+        songs.forEach((song, soundIndex) => {
+            videoScreen[soundIndex].classList.remove('phone-screen__video--active')
+            audios[soundIndex].pause()
+            videoScreen[soundIndex].pause()
+        })
     })
 }
 
-
+/*Load functions into page*/
 showVideo()
 changeHeart()
